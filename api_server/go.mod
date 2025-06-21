4666b271a151e2a8cc6a8ccb97fc6c2f7c567d4e
apple <apple@appurunoMacBook-Pro.local>
2025-06-21 10:01:11 +0900

    grpc serverを作成

--- api_server/go.mod
+++ api_server/go.mod
@@ -1,3 +1,10 @@
 module api
 
 go 1.24.0
+
+require (
+	github.com/go-sql-driver/mysql v1.9.3
+	google.golang.org/protobuf v1.36.6
+)
+
+require filippo.io/edwards25519 v1.1.0 // indirect
---
+++ api_server/internal/io_infra/grpc_server/create_grpc_server.go
@@ -0,0 +1,31 @@
+package grpc_server
+
+import (
+	"context"
+
+	pb "api/internal/grpc_gen"
+)
+
+// この型にGrpc Serverは依存する
+type IWorkflow[InsertType, ResultType any] interface {
+	Start(ctx context.Context, arg InsertType)
+	Result() (ResultType, error)
+}
+
+type TodoCreateServiceServer struct {
+	// 注入されたワークフロー　：　インターフェースで抽象度を高める
+	injected_workflow IWorkflow[*pb.CreateTodoRequest, *pb.CreateTodoResponse]
+}
+
+func NewTodoCreateServiceServer(di_workflow IWorkflow[*pb.CreateTodoRequest, *pb.CreateTodoResponse]) *TodoCreateServiceServer {
+
+	return &TodoCreateServiceServer{
+		injected_workflow: di_workflow,
+	}
+}
+
+func (tcss *TodoCreateServiceServer) Connect(ctx context.Context, request *pb.CreateTodoRequest) (*pb.CreateTodoResponse, error) {
+	workflow := tcss.injected_workflow
+	workflow.Start(ctx, request)
+	return workflow.Result()
+}
